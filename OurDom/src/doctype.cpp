ce694f94d866bf7f5f1b90b4d2d765b91b614694
Martin,Richard <martius@martiusweb.net>
2011-03-18 09:30:32 +0100

    Doctype skeleton

---
+++ OurDom/src/doctype.cpp
@@ -0,0 +1,16 @@
+/**
+ * == Analyseur XML simplifié ==
+ * @authors Hexanome 4314
+ */
+
+#include "doctype.hpp"
+
+using namespace std;
+
+namespace xml {
+
+Doctype::Doctype() {
+}
+
+}
+
---
+++ OurDom/src/doctype.hpp
@@ -0,0 +1,31 @@
+/**
+ * == Analyseur XML simplifié ==
+ * @authors Hexanome 4314
+ */
+
+#ifndef XML_DOCTYPE_H
+#define XML_DOCTYPE_H
+
+#include <istream>
+
+namespace xml {
+
+/**
+ * @brief
+ */
+class Doctype
+{
+  public:
+    /**
+     * @brief
+     */
+    Doctype();
+
+  protected:
+  private:
+};
+
+}
+
+#endif
+
